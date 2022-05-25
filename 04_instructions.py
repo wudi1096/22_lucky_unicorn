0a8e42795d3a154eee260128438a512ed92ff4ca
wudi1096 <wudi@awatapu.school.nz>
2022-05-25 12:16:07 +1200

    instruction funtion create

---
+++ 04_instructions.py
@@ -0,0 +1,32 @@
+
+def yes_no(something):
+    valid = False
+    while not valid:
+        play_game = input(something).lower()
+
+        if play_game == "yes" or play_game == "y":
+            print("program runs")
+            return play_game
+
+        elif play_game == "no" or play_game == "n":
+            print(" instruction ")
+            return play_game
+
+        else:
+            print("please enter yes or no")
+
+
+def instructions():
+    print("*** How to play ***")
+    print()
+    print("game instruction")
+    print()
+    return""
+
+
+played_before = yes_no("Have you played this game before?")
+
+if played_before =="no":
+    instructions()
+
+print("program continues")
